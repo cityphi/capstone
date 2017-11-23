67520948417cfb584e4e08df3ca2eff70f8464fc
cityphi <apennell06@gmail.com>
2017-11-23 18:05:32 -0500

    Changed matlab code

--- MATLAB/drag/curveFit.m
+++ MATLAB/drag/curveFit.m
@@ -1,7 +1,20 @@
-v=[2,4,6,8,10,12,14,16,18,20];
+v=[0,2,4,6,8,10,12,14,16,18,20];
 x=transpose(v);
-d=[0.4663,1.7320,3.5236,5.9242,9.3913,14.5235,21.3659,28.1595,35.9296,44.1682];
+d=[0,0.4663,1.7320,3.5236,5.9242,9.3913,14.5235,21.3659,28.1595,35.9296,44.1682];
 y=transpose(d);
-f=fit(x,y,'poly4')
+f=fit(x,y,'poly4');
 
-plot(f,'k',v,d,'xk'),xlabel('Velocity (m/s)'), ylabel('Drag Force (N)'), legend('Fitted Curve', 'Raw Data')
+cvalues = coeffvalues(f);
+cnames = coeffnames(f);
+output = formula(f);
+
+for ii=1:1:numel(cvalues)
+    cname = cnames{ii};
+    cvalue = num2str(cvalues(ii));
+    output = strrep(output, cname , cvalue);
+end
+
+disp(output)
+
+plot(f,'k',v,d,'xk'),xlabel('Velocity (m/s)'), ylabel('Drag Force (N)'),...
+    legend('Fitted Curve', 'Raw Data')
